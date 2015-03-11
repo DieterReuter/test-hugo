c21d6f269314a3fbc2258cbe97fae232ecead720
Dieter Reuter <dieter.reuter@me.com>
2015-03-11 22:06:34 +0100

    use master branch for deploy

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,8 @@ git config user.email "dieter.reuter@me.com"
 
 git remote add upstream "https://$GH_TOKEN@github.com/dieterreuter/dieterreuter.github.io.git"
 git fetch upstream
-git reset upstream/gh-pages
+git reset upstream/master
 
 git add -A .
 git commit -m "Drone.io build #${DRONE_BUILD_ID}, at ${GIT_COMMIT}"
-git push -q upstream HEAD:gh-pages
+git push -q upstream HEAD:master
