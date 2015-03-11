5c6d90dbb439acc92d23e81955f0aad90cb50429
Dieter Reuter <dieter.reuter@me.com>
2015-03-11 20:48:24 +0100

    use Drone.io ENV vars

--- drone.io.build.sh
+++ drone.io.build.sh
@@ -6,26 +6,16 @@ set -e
 pwd
 env
 
-# check if we're running a Drone.io or a local build
-if [ "x$GH_TOKEN" == "x" ]; then
-  DRONE_BUILD="N"
-else
-  DRONE_BUILD="Y"
-fi
-
-# detect if we are running from a master or a dev branch
-#GH_MASTER=Y
-
 
 # fetch hugo and show version in build log
-if [ "x$DRONE_BUILD" == "xY" ]; then
+if [ "x$DRONE" == "xtrue" ]; then
   go get -u -v github.com/spf13/hugo
 fi
 hugo version
 
 # build the static web content
 cd ./hugo-website
-if [ "x$GH_MASTER" == "xY" ]; then
+if [ "x$DRONE_BRANCH" == "xmaster" ]; then
   # build production posts only
   hugo --theme=hugo-uno
 else
@@ -34,7 +24,7 @@ else
 fi
 
 # push the static web content to gh-pages
-if [ "x$DRONE_BUILD" == "xY" ]; then
+if [ "x$DRONE" == "xtrue" ]; then
   echo "...push it"
 else
   echo "...local, don't push"
